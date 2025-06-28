b02d3f4978ce1a035bd71663db0321fa84616137
unknown <ducoday12345@gmail.com>
2025-06-28 13:27:25 +0700

    'installing-amplify'

--- amplify/addcontent.ts
+++ amplify/addcontent.ts
@@ -2,7 +2,7 @@
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
-const region = process.env.AWS_REGION!;
+const region = process.env.REGION!;
 const credentials = {
         accessKeyId: process.env.ACCESS_KEY_ID!,
         secretAccessKey: process.env.SECRET_ACCESS_KEY!,
